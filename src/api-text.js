9df5e563c5e698d0232321ee67c213031595f23b
Sheila Bonilla <shbonillapl@gmail.com>
2024-08-05 20:29:29 +0200

    add new config api

--- src/api-text.js
+++ src/api-text.js
@@ -4,6 +4,8 @@ const fs = require("fs");
 
 // Access your API key as an environment variable (see "Set up your API key" above)
 const genAI = new GoogleGenerativeAI(process.env.API_KEY);
+const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
+
 
 // Converts local file information to a GoogleGenerativeAI.Part object.
 function fileToGenerativePart(path, mimeType) {
@@ -15,41 +17,43 @@ function fileToGenerativePart(path, mimeType) {
   };
 }
 
-async function run() {
-  // The Gemini 1.5 models are versatile and work with both text-only and multimodal prompts
-  const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
-
-  const prompt = "Hazme un resumen en español";
-  const prompt1 = "Hazme un examen con preguntas y respuestas en español";
-  const prompt2 = "Ponme un ejercicio por cada capítulo";
-
-
-  const pdf = [
-    fileToGenerativePart("./libro.pdf", "application/pdf"),
-  ];
-
-  const result = await model.generateContent([prompt, ...pdf]);
-  const response = await result.response;
-  const text = response.text();
-  console.log(text);
-
-  console.log ("-------------------------------------------------------------------");
-
-  const result1 = await model.generateContent([prompt1, ...pdf]);
-  const response1 = await result1.response;
-  const text1 = response1.text();
-  console.log(text1);
-
-  console.log ("-------------------------------------------------------------------");
-
-
-  const result2 = await model.generateContent([prompt2, ...pdf]);
-  const response2 = await result2.response;
-  const text2 = response2.text();
-  console.log(text2);
-
+async function resume(name){
+    const prompt = "Hazme un resumen en español";
+    const pdf = [
+        fileToGenerativePart("./uploads/"+name, "application/pdf"),
+      ];
+      const result = await model.generateContent([prompt, ...pdf]);
+      const response = await result.response;
+      const text = response.text();
+      console.log(text);
+      return text
 }
 
-module.exports = {run}
+async function exam(name){
+    const prompt = "Hazme un examen con preguntas y respuestas en español";
+    const pdf = [
+        fileToGenerativePart("./uploads/"+name, "application/pdf"),
+      ];
+      const result = await model.generateContent([prompt, ...pdf]);
+      const response = await result.response;
+      const text = response.text();
+      console.log(text);
+      return text
+}
+
+async function exercisebychapter(name){
+    const prompt = "Ponme un ejercicio por cada capítulo";
+    const pdf = [
+        fileToGenerativePart("./uploads/"+name, "application/pdf"),
+      ];
+      const result = await model.generateContent([prompt, ...pdf]);
+      const response = await result.response;
+      const text = response.text();
+      console.log(text);
+      return text
+}
+
+
+module.exports = {resume, exam,exercisebychapter}
 
 
--- src/index.js
+++ src/index.js
@@ -1,11 +1,55 @@
 const express = require('express')
-//const {run} =  require("./api.js");
-const {run} =  require("./api-text.js");
+const {resume, exam, exercisebychapter} =  require("./api-text.js");
 const app = express();
+const multer = require('multer');
 
-app.get('/', (req, res) => {
-  res.send('¡Hola Mundo!');
-  run()
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, 'uploads/'); // Directorio donde se guardarán los archivos
+  },
+  filename: (req, file, cb) => {
+    cb(null, Date.now() + '-' + file.originalname); // Nombre del archivo en el servidor
+  },
+});
+
+const upload = multer({ storage: storage });
+
+app.post('/upload', upload.single('archivo'), (req, res) => { // Primera petición necesaria para cargar el archivo
+  const archivo = req.file;
+  if (!archivo) {
+    return res.status(400).json({ mensaje: 'No se recibió ningún archivo' });
+  }
+  res.json({ mensaje: 'Archivo recibido con éxito', archivo });
+});
+
+app.get('/resume/:name', async(req, res) => { //Get de un resumen
+  if(req.params.name){
+    const result = await resume(req.params.name)
+    res.send(result)
+  }
+  else{
+    res.send('No se ha podido crear el resumen');
+  }
+});
+
+app.get('/exam/:name', async(req, res) => { // Get de un examen
+  if(req.params.name){
+    const result = await exam(req.params.name)
+    res.send(result)
+  }
+  else{
+    res.send('No se ha podido crear el examen');
+  }
+});
+
+app.get('/exercisebychapter/:name', async(req, res) => { //Get de ejercicios por capítulos
+  if(req.params.name){
+    const result = await exercisebychapter(req.params.name)
+    res.send(result)
+  }
+  else{
+    res.send('No se han podido crear los ejercicios');
+  }
 });
 
 app.listen(3000, async() => {
