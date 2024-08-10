de7298ed97f5a5b3fc8776908967fe5211306db8
SergioRBurdalo <>
2024-08-10 20:54:09 +0200

    save document with modal name

--- src/index.js
+++ src/index.js
@@ -16,7 +16,7 @@ const storage = multer.diskStorage({
     cb(null, uploadDir);
   },
   filename: (req, file, cb) => {
-    cb(null, Date.now() + '-' + file.originalname);
+    cb(null, file.originalname);
   },
 });
 
